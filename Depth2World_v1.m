246c06c02fcae8aa3adac3270f8cbf524dce2603
nakini <tnb88@mail.missouri.edu>
2017-04-21 19:31:31 -0500

    Only realigned the comments in the code

--- Depth2World_v1.m
+++ Depth2World_v1.m
@@ -1,21 +1,22 @@
 function [Xw, Yw, Zw] = Depth2World_v1(depthInMeters, maxDepth)
-% This function converts the depth values in meters to world coordinates. The input is a
-% MxN matrix and the out will be M*Nx3 matrix. To convert the depth into world coordiantes
-% we need the instrinsic parameters of the depth camera. And if we want to translate them
-% to the RGB camera frame we also need extrinsic parameters.
+% This function converts the depth values in meters to world coordinates. The 
+% input is a MxN matrix and the out will be M*Nx3 matrix. To convert the depth 
+% into world coordiantes we need the instrinsic parameters of the depth camera. 
+% And if we want to translate them to the RGB camera frame we also need 
+% extrinsic parameters.
 
 if (nargin < 2)
     maxDepth = 3;
 end
-% Extrinsic parameters of the depth camera. These values are obtained from Herrera's TPAMI
-% 2012 paper.
+% Extrinsic parameters of the depth camera. These values are obtained from 
+% Herrera's TPAMI 2012 paper.
 fx_d = 579.83;      % Focal length in X and Y
 fy_d = 586.73;
 cx_d = 321.55;      % Principle point in X and Y
 cy_d = 235.01;
 
-% Find all the values which are in between .3 to 3 meters. We could extend the upper
-% limit till 3.5 meters, as this is the upper limit of the kinect.
+% Find all the values which are in between .3 to 3 meters. We could extend the
+% upper limit till 3.5 meters, as this is the upper limit of the kinect.
 [row col] = find(depthInMeters > 0.3 & depthInMeters < maxDepth);
 indxValues = sub2ind(size(depthInMeters), row, col);
 
@@ -25,8 +26,8 @@ Zd = depthInMeters(indxValues);
 Xd = (row - cx_d) .* Zd / fx_d;
 Yd = (col - cy_d) .* Zd / fy_d;
 
-% Kinect is coordinate frame is different from the regular world frame. So first transform
-% all the point from the kinect frame to regular world frame.
+% Kinect is coordinate frame is different from the regular world frame. So first 
+% transform all the point from the kinect frame to regular world frame.
 Xw = Yd;
 Yw = Zd;
 Zw = -Xd;
