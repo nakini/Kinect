eda4f96e14f01d487f67951a1122785eab947164
nakini <tnb88@mail.missouri.edu>
2018-07-12 14:37:38 -0500

    Accept the IR image name from the user instead of a hardcoded name

--- BatchUIntImg2DoubleImg.m
+++ BatchUIntImg2DoubleImg.m
@@ -1,14 +1,20 @@
-function BatchUIntImg2DoubleImg(dirName, fileNum)
+function BatchUIntImg2DoubleImg(dirName, fileNum, imgName)
 % This function convertes all the images in a floder. The original images 
 % are of type uint16 and the converted images are of type double.
 %
+% INPUTs:
+%   dirName = Directory containing all the IR images
+%   fileNum = The image sequence numbers that are to be processed
+%   imgName = Name of the image file without sequence numbers
+%
 % EXAMPLE:
 %   dirName = '/home/fovea/tnb88/Documents/ViGIR_Research/PhD/Calibration/Kinect_v2_20170107/IR2';
-%   batchUIntImg2DoubleImg(dirName, 1:10)
+%   batchUIntImg2DoubleImg(dirName, 1:10, 'ir_11-')
+
 
 totalFiles = length(fileNum);
 for iTF = 1:totalFiles
-    fileName = sprintf('%s/irImg_%d.pgm', dirName, fileNum(iTF));
+    fileName = sprintf('%s/%s%d.pgm', dirName, imgName, fileNum(iTF));
     imgUInt16 = imread(fileName);
     imgDouble = im2double(imgUInt16);
     imwrite(imgDouble, fileName)
