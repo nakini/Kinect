f81f5086cf3bd1a8fd81391bae395616008727c9
nakini <tnb88@mail.missouri.edu>
2018-11-28 13:55:44 -0600

    Get rid of change in Y and the mode
    For some reason, I was deducting the maximum value from the Y axis which I fixed.
    And the other part is mode which was suppose to be used for the data stored in
    the text files. I never used it so got rid of it.

--- ConvertRawDepth2Ply.m
+++ ConvertRawDepth2Ply.m
@@ -1,5 +1,5 @@
 function ConvertRawDepth2Ply(dirName, maxDepthInMeters, KinectType, ...
-                            startIndx, endIndx, samplingRate, CenterFlag, Mode)
+                            startIndx, endIndx, samplingRate, CenterFlag)
 % This function reads the text files which contain the raw depth and converts 
 % them into a ply file. It also creates XYZ, Nor, Tri files for 3D model 
 % creation.
@@ -27,28 +27,22 @@ function ConvertRawDepth2Ply(dirName, maxDepthInMeters, KinectType, ...
 %------------------------------------------------------------------------------
 %------------------------------- START ----------------------------------------
 % Set the default parameters firs.
-if (nargin < 8)
-    % Read image files.
-    Mode = 2;
-    if (nargin < 7)
-        CenterFlag = 'No Center';
-        if (nargin < 6)
-            % Sampling rate -- Using kinect we can grab a lot a images but we don't
-            % need all of them to create the complete point cloud. So, take only
-            % few samples from the whole data set.
-            samplingRate = 1;
-            if (nargin < 5)
-                % First get all the depth image files inside the directory.
-                if (strcmpi(KinectType, 'v1'))
-                    listTxtFiles = dir([dirName, '/*.ppm']);
-                elseif (strcmpi(KinectType, 'v2'))
-                    listTxtFiles = dir([dirName, '/*.png']);
-                end
-                endIndx = length(listTxtFiles);
-                
+if (nargin < 7)
+    CenterFlag = 'No Center';
+    if (nargin < 6)
+        % Sampling rate -- Using kinect we can grab a lot a images but we don't
+        % need all of them to create the complete point cloud. So, take only
+        % few samples from the whole data set.
+        samplingRate = 1;
+        if (nargin < 5)
+            % First get all the depth image files inside the directory.
+            if (strcmpi(KinectType, 'v1'))
+                listTxtFiles = dir([dirName, '/*.ppm']);
+            elseif (strcmpi(KinectType, 'v2'))
+                listTxtFiles = dir([dirName, '/*.png']);
             end
+            endIndx = length(listTxtFiles);
         end
-        
     end
 else
     error(['At least provide DIRECTORY-NAME containing the images, ', ...
@@ -63,41 +57,33 @@ system(sprintf('mkdir %s/PCinXYZNorTri', dirName));
 % into a X, Y, and Z coordinates and then create a ply file from the 
 % coordinates. In the store the ply file in the newly created directory.
 for iNTF=startIndx:samplingRate:endIndx
-    if Mode == 1
-        % Read the text file. Each text file contains a series of float values,
-        % seperated by a ','.
-        txtFileName = sprintf('rawDepth%04d.txt', iNTF);
-        depthRaw = textread([dirName, '/', txtFileName], '', 'delimiter', ',');
-    elseif Mode == 2
-        % Read the ppm files. Each pixel in the ppm file is a depth value.
-        if (strcmpi(KinectType, 'v1'))
-            ppmFileName = sprintf('depth%04d.ppm', iNTF);
-            compFileName = [dirName, '/', ppmFileName];
-            if (exist(compFileName, 'file') == 2)
-                depthRaw = imread(compFileName);
-            else
-                continue;
-            end      
-            % Convert the raw depth into depth in meters.
-            depthInMeters = RawDepth2Meters_v1(depthRaw);
-            % Now, get the X, Y, Z of each point in a world coordinate frame.
-            [Xw, Yw, Zw] = Depth2World_v1(depthInMeters, maxDepthInMeters);
-        elseif (strcmpi(KinectType, 'v2'))
-            pngFileName = sprintf('depthImg_%04d.png', iNTF);
-            tmpFileName = [dirName, '/', pngFileName];
-            if(exist(tmpFileName, 'file') == 2)
-                % Now, get the X, Y, Z of each point in a world coordinate frame.
-                [Xw, Yw, Zw] = Depth2World_v2(tmpFileName, maxDepthInMeters);
-            else
-                continue;
-            end
+    % Read the ppm files. Each pixel in the ppm file is a depth value.
+    if (strcmpi(KinectType, 'v1'))
+        ppmFileName = sprintf('depth%04d.ppm', iNTF);
+        compFileName = [dirName, '/', ppmFileName];
+        if (exist(compFileName, 'file') == 2)
+            depthRaw = imread(compFileName);
         else
-            error('Kinect type should either v1 or v2');
+            continue;
         end
-        
+        % Convert the raw depth into depth in meters.
+        depthInMeters = RawDepth2Meters_v1(depthRaw);
+        % Now, get the X, Y, Z of each point in a world coordinate frame.
+        [Xw, Yw, Zw] = Depth2World_v1(depthInMeters, maxDepthInMeters);
+    elseif (strcmpi(KinectType, 'v2'))
+        pngFileName = sprintf('depthImg_%04d.png', iNTF);
+        tmpFileName = [dirName, '/', pngFileName];
+        if(exist(tmpFileName, 'file') == 2)
+            % Now, get the X, Y, Z of each point in a world coordinate frame.
+            [Xw, Yw, Zw] = Depth2World_v2(tmpFileName, maxDepthInMeters);
+        else
+            continue;
+        end
+    else
+        error('Kinect type should either v1 or v2');
     end
-%     dataXYZ = cat(2, Xw, Yw, Zw);
-    dataXYZ = cat(2, Xw, Yw-maxDepthInMeters, Zw);
+    dataXYZ = cat(2, Xw, Yw, Zw);
+ 
     % Center the point cloud, i.e., make the mean of the pc zero.
     if (strcmpi(CenterFlag , 'Center'))
         dataXYZ = dataXYZ - mean(dataXYZ);
@@ -108,22 +94,17 @@ for iNTF=startIndx:samplingRate:endIndx
     if ~isempty(dataXYZ)
         % Create a ply file out of these point.
         % Get rid of the file extension.
-        if Mode ==1
-            nameWithoutExt = strrep(txtFileName, '.txt', '');
-        elseif Mode ==2
-            if (strcmpi(KinectType, 'v1'))
-                nameWithoutExt = strrep(ppmFileName, '.ppm', '');
-            elseif (strcmpi(KinectType, 'v2'))
-                nameWithoutExt = strrep(pngFileName, '.png', '');
-            else
-                error('Kinect type should either v1 or v2');
-            end
+        if (strcmpi(KinectType, 'v1'))
+            nameWithoutExt = strrep(ppmFileName, '.ppm', '');
+        elseif (strcmpi(KinectType, 'v2'))
+            nameWithoutExt = strrep(pngFileName, '.png', '');
+        else
+            error('Kinect type should either v1 or v2');
         end
         fileName = [dirName, '/PCinPLY/', nameWithoutExt];
         pcwrite(pointCloud(dataXYZ), fileName);
-%         pc2ply(fileName, dataXYZ);
         
-        % Create XYZ, Nor and Tri files for each point cloud which could be used 
+        % Create XYZ, Nor and Tri files for each point cloud which could be used
         % to register to with each other to create a complete 3D point cloud.
         binDir = [dirName, '/PCinXYZNorTri/'];
         CreateXYZTriNor(dataXYZ, binDir, iNTF);
