51b059f8d66fa41d5979fdbe069182a1f313b83f
nakini <tnb88@mail.missouri.edu>
2019-09-24 13:57:14 -0500

    Rename the file and load the point clouds

--- EstimateTformMatchingRGBs.m
+++ EstimateTformMatchingRGBs.m
@@ -1,4 +1,4 @@
-function FindTransformation(dirName, startIndx, endIndx, geomEstParams)
+function EstimateTformMatchingRGBs(dirName, startIndx, endIndx, pcDirName, geomEstParams)
 % In this function, I am going to read two images from a given folder then
 % estimate the transformation between the two using the RGB images and their
 % corresponding point clouds.
@@ -7,6 +7,10 @@ function FindTransformation(dirName, startIndx, endIndx, geomEstParams)
 %       corresponding ply files.
 %   startIndx       := File number of 1st pc
 %   endIndx         := Last file number
+%   pcDirName       := Directory that contains the 3D point clouds in ply format
+%   geomEstParams   := Parameters for matching the two images and removing
+%       outlier in pair of matching points.
+%
 % OUTPUT(s):
 %
 % Example:
@@ -21,6 +25,10 @@ if nargin < 3
     error('Provide the directory name containing the images');
 end
 if nargin < 4
+    % This is the default name I have been using
+    pcDirName = 'PCinPLY';
+end
+if nargin < 5
     geomEstParams = struct('tformType', 'projective', 'maxDist', 3.5);
 end
 
@@ -28,18 +36,21 @@ end
 while startIndx < endIndx
     % In the begining, make the 1st found image as the anchor image. After that,
     % however, make the 'moved' image as the achor image.
-    rgbNameAnch = ['rgbImg_', num2str(startIndx), '.jpg'];
+    anchNum = startIndx;
+    rgbNameAnch = ['rgbImg_', num2str(anchNum), '.jpg'];
     rgbFullNameAnch = [dirName, '/', rgbNameAnch];
     
-    % IF the file is available read it and set it as the Anchor point cloud.
+    % IF the file is available read it and set it as the Anchor image and then
+    % look for the Moved image.
     if exist(rgbFullNameAnch, 'file') == 2
-        rgbImgAnch = imread(rgbFullNameAnch);
+        rgbImgAnch = imread(rgbFullNameAnch);               % Read 1st image
         
         % Now, look for the next RGB image
         startIndx = startIndx + 1;
         while startIndx < endIndx
+            movedNum = startIndx;
             % Read anchored and moved point cloud
-            rgbNameMoved = ['rgbImg_', num2str(startIndx), '.jpg'];
+            rgbNameMoved = ['rgbImg_', num2str(movedNum), '.jpg'];
             rgbNameFullMoved = [dirName, '/', rgbNameMoved];
             
             % IF the file is found then set it as the moved point cloud and then
@@ -49,12 +60,18 @@ while startIndx < endIndx
                 fprintf('Matching -- %s and %s\n\n', rgbNameAnch, rgbNameMoved);
                 
                 % Read the second point cloud
-                rgbImgMoved = imread(rgbNameFullMoved);
+                rgbImgMoved = imread(rgbNameFullMoved);     % Read 2nd image
                 
                 % Find the matching point between two images.
                 matchTech = 'SURF';
                 [inlierPts1, inlierPts2] = FindMatchedPoints(rgbImgAnch, ...
                     rgbImgMoved, matchTech, geomEstParams, 1);
+                
+                % Now read the 3D point clouds
+                pcNameAnch = ['depthImg_', num2str(anchNum), '.ply'];
+                pcFullNameAnch = [dirName, '/', pcDirName, '/', pcNameAnch];
+                pcNameMoved = ['depthImg_', num2str(movedNum), '.ply'];
+                pcFullNameMoved = [dirName, '/', pcDirName, '/', pcNameMoved];
                 
                 break;
             else
