0271914f1fb22bf83f4dfdbf87495fc9e583ad76
nakini <tnb88@mail.missouri.edu>
2019-10-02 11:14:57 -0500

    Automatically registers two pcs

--- EstimateTform_Batch.m
+++ EstimateTform_Batch.m
@@ -1,4 +1,5 @@
-function EstimateTformMatchingRGBs(dirName, startIndx, endIndx, pcDirName, geomEstParams)
+function EstimateTform_Batch(dirName, startIndx, endIndx, pcDirName, ...
+    geomEstParams, calibStereo, dispFlag)
 % In this function, I am going to read two images from a given folder then
 % estimate the transformation between the two using the RGB images and their
 % corresponding point clouds.
@@ -10,6 +11,8 @@ function EstimateTformMatchingRGBs(dirName, startIndx, endIndx, pcDirName, geomE
 %   pcDirName       := Directory that contains the 3D point clouds in ply format
 %   geomEstParams   := Parameters for matching the two images and removing
 %       outlier in pair of matching points.
+%   calibStereo     := Stereo calibration parameters between IR and RGB of the 
+%       Kinect that was used to collect the data.
 %
 % OUTPUT(s):
 %
@@ -20,16 +23,28 @@ function EstimateTformMatchingRGBs(dirName, startIndx, endIndx, pcDirName, geomE
 
 %------------------------------------------------------------------------------
 %------------------------------- START ----------------------------------------
-
-if nargin < 3
+% Directory cotaining the images
+if nargin < 3               
     error('Provide the directory name containing the images');
 end
-if nargin < 4
+% Name of the folder where point clouds will be saved
+if nargin < 4 || isempty(pcDirName)
     % This is the default name I have been using
     pcDirName = 'PCinPLY';
 end
-if nargin < 5
+% Parameters to be used to match 2 rgb images
+if nargin < 5 || isempty(geomEstParams)
     geomEstParams = struct('tformType', 'projective', 'maxDist', 3.5);
+end
+% Calibration parameters of the Kinect sensors
+if nargin < 6 || isempty(calibStereo)
+    calibStereo = ['~/Dropbox/PhD/Data/Calibration/', ...
+        'Calibration_20181114/HandHeld/Calib_Results_stereo_rgb_to_ir.mat'];
+    disp('WARNING!!! -- Loading the handheld kinect parameters');
+end
+% Display pair of point clouds.
+if nargin < 7 || isempty(dispFlag)
+    dispFlag = 0;       % By default don't display the point clouds
 end
 
 % Load a pair of images at a time and find the correspondence.
@@ -64,15 +79,49 @@ while startIndx < endIndx
                 
                 % Find the matching point between two images.
                 matchTech = 'SURF';
-                [inlierPts1, inlierPts2] = FindMatchedPoints(rgbImgAnch, ...
-                    rgbImgMoved, matchTech, geomEstParams, 1);
+                [inlierPtsAnch, inlierPtsMoved] = FindMatchedPoints(...
+                    rgbImgAnch, rgbImgMoved, matchTech, geomEstParams, 1);
                 
                 % Now read the 3D point clouds
                 pcNameAnch = ['depthImg_', num2str(anchNum), '.ply'];
                 pcFullNameAnch = [dirName, '/', pcDirName, '/', pcNameAnch];
+                pcAnch = pcread(pcFullNameAnch);
                 pcNameMoved = ['depthImg_', num2str(movedNum), '.ply'];
                 pcFullNameMoved = [dirName, '/', pcDirName, '/', pcNameMoved];
+                pcMoved = pcread(pcFullNameMoved);
                 
+                % Load the transformaion parameters. If you have Depth-to-RGB
+                % then use it directly or else take the inverse of RGB-to-Depth
+                % parameters.
+                load(calibStereo, 'R', 'T', 'KK_left', 'KK_right');
+                % Create as strucutre that will hold R matrix and T vector only.
+                tformDepth2RGB.R = inv(R);
+                % Convert into Meters as the PC is in Meters
+                tformDepth2RGB.T = -inv(R)*T/1000;
+                tformDepth2RGB.KK_RGB = KK_left;
+                tformDepth2RGB.KK_IR = KK_right;
+                
+                % Estimate the transfomation from the two point cloud using the
+                % RGB matchin points.
+                pcStructAnch = struct('rgbPts', inlierPtsAnch, 'pc', pcAnch, ...
+                    'tformDepth2RGB', tformDepth2RGB);
+                pcStructMoved = struct('rgbPts', inlierPtsMoved, 'pc', pcMoved, ...
+                    'tformDepth2RGB', tformDepth2RGB);
+                tformMoved2Anchor = EstimateTformMatchingRGB(pcStructAnch, ...
+                    pcStructMoved);
+                
+                % Save the transformation matrix into a file
+                rtNameMoved = ['rt_', num2str(movedNum), '.txt'];
+                rtFullNameMoved = [dirName, '/', pcDirName, '/', rtNameMoved];
+                WriteRT(tformMoved2Anchor, rtFullNameMoved);
+                
+                % If needed display the point cloud
+                if dispFlag == 1
+                    pcMoved_Tformed = TransformPointCloud(pcMoved, ...
+                        tformMoved2Anchor);
+                    pcshowpair(pcAnch, pcMoved_Tformed);
+                    legend('Anchor PC', 'Transformed PC');
+                end
                 break;
             else
                 % Go to the next image
@@ -84,15 +133,3 @@ while startIndx < endIndx
         startIndx = startIndx + 1;
     end
 end
-
-        
-        
-        
-
-% Now, read the corresponding two ply files along with the depth images
-
-% Then find out the 3D points corresponding to each RGB image pixel
-
-% Then estimate the transformation between the two point clouds.
-
-% In the end, display the two registered point clouds.
