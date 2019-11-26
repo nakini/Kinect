4ca2fd934a2f6a77bac2558344c726f293313d7d
nakini <tnb88@mail.missouri.edu>
2019-11-26 14:10:10 -0600

    Two different ways to get RTs; Also update input args
    Either get RTs in a table as user input or read test files or create a table.
    Reading from file doesn't include the loop closure, need to fix it in future.
    The input argument matchInfo now holds all the information needed for bundle
    adjustment.

--- BundleAdjustment_Batch.m
+++ BundleAdjustment_Batch.m
@@ -1,8 +1,9 @@
 function [refinedStruct, rawStruct] = BundleAdjustment_Batch(dirStruct, ...
-    matchInfo, matchPtsPxls, rtRaw, varargin)
+    matchInfo, varargin)
 % In this function, I am going to read all the point clouds along with the RGB
 % images and the pair wise matching pixels and carry out the bundle adjustment
-% to optimize the transformation parameters and the 3D points.
+% to optimize the transformation parameters and the 3D points. This includes the
+% "loop-clouser" too.
 %
 % INPUT(s)
 % ========
@@ -14,46 +15,56 @@ function [refinedStruct, rawStruct] = BundleAdjustment_Batch(dirStruct, ...
 %	3) rtFolderName -- Name of the folder relative to 'dirName' holding all the
 %	text files which contain the R|T information.
 %
-% 2. matchInfo: Mx3 table storing RGB image names, number of matched points of 
-% each image with the previous one and the "rmse" value from rigid registration.
-% The count and rmse for the 1st image will be 0 and 0, respectively.
+% 2. matchInfo: Mx8 table
+%   1) Anchor, Moved -- Image names of the anchor and moved image, respectively
+%   2) Matched_Points -- Number of matched points of between two images
+%   3) ICP_RMSE -- the "rmse" value from rigid registration
+%   4) PtsPxls_Anch, PtsPxls_Moved -- Matched pixles info of the anchor and the
+%   moved image, respectively. And each element of the column is a structure,
+%   which holds the following fields.
+%       a) indxPC -- Px1 vector of indices of matched 3D points of point cloud
+%       b) pixelsRGB -- Px2 matrix of 2D pixels of matched RGB image
+%   5) Anchor_ViewID, Moved_ViewID -- View ids of anchor and moved pc for each
+%   pair, respectively.
 %
-% 3. matchPtsPxls: Mx2 Cell storing M pairs of structures. Each structure in the
-% pair, holds the following fields.
-%   1) indxPC -- Px1 vector of indices of matched 3D points of point cloud
-%   2) pixelsRGB -- Px2 matrix of 2D pixels of matched RGB image
+% 3. ['rtRaw', rtRaw]: (M+1)x3 table
+%   1) ViewId: View-id of each point cloud
+%   2) Orientation: Rotation matrix corresponding to each view
+%   3) Location: Translation of each view
+% The last view will have 2 transformations w.r.t to the base -- One through the
+% previous point cloud and the other is direct to the base pc.
 %
-% 4. ['calibStereo', calibStereo]: Mat-file holding stereo calibration parameters 
-% between IR and RGB of the Kinect that was used to collect the data.
+% 4. ['calibStereo', calibStereo]: Mat-file holding stereo calibration
+% parameters between IR and RGB of the Kinect that was used to collect the data.
 %
 % OUTPUT(s)
 % =========
-% 1. refinedStruct, rawStruct: These two structs hold the following the
-% non-refined and refinded info, respectively. Each structure has:
+% 1. refinedStruct, rawStruct: These two structs hold the following the refinded
+% and non-refined info, respectively. Each structure has:
 %   1) xyz -- Px3 3D points from all views
 %   2) rt -- Px3 table of transformation matrices for all the views
 %
-% Example(s):
+% Example(s)
+% ==========
 %
-%------------------------------------------------------------------------------
-%------------------------------- START ----------------------------------------
+%-------------------------------------------------------------------------------
+%------------------------------- START -----------------------------------------
 
-% Validate input arguments ----------------------------------------------------
+% Validate input arguments -----------------------------------------------------
 p = inputParser;
 p.StructExpand = false;                 % Accept strutcture as one element
 
-% Parameters to be used to match 2 RGB images
 % Calibration parameters of the Kinect sensors
 defaultCalibStereo = ['~/Dropbox/PhD/Data/Calibration/Calibration_20181114/', ...
     'HandHeld/Calib_Results_stereo_rgb_to_ir.mat'];
+defaultRtFalg = true;                   % Assume input RTs are not given by user
 
 addRequired(p, 'dirStruct', @validateDirStruct);
 addRequired(p, 'matchInfo', @validdateMatchInfo);
-addRequired(p, 'matchPtsPxls', @validateMatchPtsPxls);
-addRequired(p, 'rtInfo', @validateRtInfo);
+addParameter(p, 'rtRaw', defaultRtFalg, @validateRtInfo);
 addParameter(p, 'calibStereo', defaultCalibStereo, @validateCalibStereo);
 
-p.parse(dirStruct, matchInfo, matchPtsPxls, rtRaw, varargin{:});
+p.parse(dirStruct, matchInfo, varargin{:});
 disp(p.Results);
 
 % Store variales into local variables to save typing
@@ -62,9 +73,15 @@ rtFolderName = dirStruct.rtFolderName;
 plyFolderName = dirStruct.plyFolderName;
 calibStereo = p.Results.calibStereo;
 matchInfo = p.Results.matchInfo;
-matchPtsPxls = p.Results.matchPtsPxls;
+rtRaw = p.Results.rtRaw;
+if ~istable(rtRaw)
+    defaultRtFalg = false;
+    disp('Reading RTs from the text files');
+else
+    disp('Reading RTs provided by the user');
+end
 
-% Algorithm -------------------------------------------------------------------
+% Algorithm --------------------------------------------------------------------
 % Create the respective lists that will hold all the 3D and 2D points for all 
 % images.
 totalPts = sum(matchInfo.Matched_Points);   % Total number points from all images
@@ -73,18 +90,22 @@ pxlList(1, 1:totalPts) = pointTrack;        % 2D pixel list of pointTrack objs
 
 % Go through each pair and load the transformation matrix, the point cloud
 numImgs = size(matchInfo, 1);
-rtRaw = cell(numImgs, 3);                  % Store R and T along with the index
-rtRaw(1, :) = {1, eye(3,3), [0, 0, 0]};    % 1st image is the anchor
-endIndx = 0;
-for iImg = 2:numImgs                       % Ignore the 1st pair which is empty
-    indxAnch = iImg - 1;
-    indxMoved = iImg;
-    
-    numMtchPts = matchInfo.Matched_Points(indxMoved, 1);    % Number of points
-    anchName = matchInfo.Name(indxAnch, 1);                 % Name of the pc
-    movedName = matchInfo.Name(indxMoved, 1);
-    startIndx = endIndx + 1;
-    endIndx  = endIndx + numMtchPts;
+
+% Container to store R and T values along with the veiw-id of each view if it is
+% not provided by the user. Also add one more view to close the loop.
+if defaultRtFalg == false
+    rtRaw = cell(numImgs+1, 3);
+    rtRaw(1, :) = {1, eye(3,3), [0, 0, 0]};     % 1st image is the anchor
+end
+
+endIndxMatchPts = 0;
+baseName = matchInfo.Anchor(1);             % Every pc is represented w.r.t 1st one
+for iImg = 1:numImgs
+    numMtchPts = matchInfo.Matched_Points(iImg, 1);     % Number of points
+    anchName = matchInfo.Anchor(iImg);                  % Name of the pc
+    movedName = matchInfo.Moved(iImg);
+    startIndxMatchPts = endIndxMatchPts + 1;
+    endIndxMatchPts  = endIndxMatchPts + numMtchPts;
     
     % Read the save point clouds
     % ==========================
@@ -96,38 +117,47 @@ for iImg = 2:numImgs                       % Ignore the 1st pair which is empty
     % Load the pcs and segregate the matching points from each pair
     pcAnch = pcread(pcFullNameAnch);
     pcMoved = pcread(pcFullNameMoved);
-    pcAnchMtcPts = pcAnch.Location(matchPtsPxls{iImg, 1}.indxPC, :);
-    pcMovedMtcPts = pcMoved.Location(matchPtsPxls{iImg, 2}.indxPC, :);
+    pcAnchMtcPts = pcAnch.Location(matchInfo.PtsPxls_Anch{iImg}.indxPC, :);
+    pcMovedMtcPts = pcMoved.Location(matchInfo.PtsPxls_Moved{iImg}.indxPC, :);
     
 	% Store the matching 3D points list
-    xyzRaw(startIndx:endIndx, :) = pcMovedMtcPts;
+    xyzRaw(startIndxMatchPts:endIndxMatchPts, :) = pcMovedMtcPts;
     
     % Create pointTrack object for each pixel pair
     % ============================================
     % Save the 2D pixel points into a pointTrack class -- It takes the multiple
     % pixels from multiple views and the view-ids and creates an object.
-    pxlAnch = matchPtsPxls{iImg, 1}.pixelsRGB;
-    pxlMoved = matchPtsPxls{iImg, 2}.pixelsRGB;
-    viewIds = [indxAnch, indxMoved];
+    pxlAnch = matchInfo.PtsPxls_Anch{iImg}.pixelsRGB;
+    pxlMoved = matchInfo.PtsPxls_Moved{iImg}.pixelsRGB;
+    currViewIDs = [matchInfo.Anchor_ViewID(iImg), matchInfo.Moved_ViewID(iImg)];
     for iTrk = 1:numMtchPts
-        pxlList(1, startIndx+iTrk-1) = pointTrack(viewIds, ...
+        pxlList(1, startIndxMatchPts+iTrk-1) = pointTrack(currViewIDs, ...
             [pxlAnch(iTrk, :); pxlMoved(iTrk, :)]);
     end
     
     % Read R|T files
     % ==============
-    % Read the rotation and translation from the text files and update the
-    % "rtInfo" table
-    tmpRTName = [dirName, '/', rtFolderName, '/rt_'];
-    rtFullName = [tmpRTName, char(movedName), '.txt'];
-    % Read the text file which will return a sturct with R(3x3), and T(3,1)
-    tformMoved2Anchor = ReadRT(rtFullName);
-    rtRaw(indxMoved, :) = {indxMoved, tformMoved2Anchor.R, tformMoved2Anchor.T'};
+    % Read the rotation and translation from the text files if the "rtRaw" table
+    % is not provided by the user.
+    if defaultRtFalg == false
+        rtFullName = [dirName, '/', rtFolderName, '/Absolute/rt_', ...
+            char(movedName), '_to_', char(baseName), '.txt'];
+        % Read the text file which will return a sturct with R(3x3), and T(3,1)
+        [tformMoved2Anchor, fileReadFlag] = ReadRT(rtFullName);
+        if fileReadFlag == true
+            rtRaw(iImg+1, :) = {matchInfo.Moved_ViewID(iImg), ...
+                tformMoved2Anchor.R, tformMoved2Anchor.T'};
+        else
+            error("Unable to read the RT file!!!");
+        end
+    end
 end
 
-% Create a table for R|T
-rtRaw = table(cell2mat(rtRaw(:,1)), rtRaw(:,2), rtRaw(:,3), ...
-    'VariableNames', {'ViewId', 'Orientation', 'Location'});
+% Create a table for R|T if not provided by the user
+if defaultRtFalg == false
+    rtRaw = table(cell2mat(rtRaw(:,1)), rtRaw(:,2), rtRaw(:,3), ...
+        'VariableNames', {'ViewId', 'Orientation', 'Location'});
+end
 
 % Load the camera intrinsic parameters and create an object
 load(calibStereo, 'KK_left', 'KK_right');
@@ -147,25 +177,22 @@ rtRaw.ViewId = uint32(rtRaw.ViewId);	% cameraPoses only takes IDs as uint32
 % ==========
 % Go through all the rt_*.txt files and update it with the current finding of
 % bundle adjustments.
-for iN = 1:numImgs
+for iN = 2:numImgs+1
     % Create a file name for RT
-    tmpRTName = [dirStruct.dirName, '/', dirStruct.rtFolderName, '/rt_'];
-    rtNum = matchInfo.Name(iN, 1);
-    rtFullName = [tmpRTName, char(rtNum), '.txt'];
-    
-    % Read the text file which will return a sturct with R(3x3), and T(3,1)
-    tformMoved2Anchor = ReadRT(rtFullName);
+    rtFullName = [dirName, '/', rtFolderName, '/Absolute/rt_',  ...
+        char(matchInfo.Moved(iN-1)), '_to_', char(baseName), '.txt'];
     
     % Update the current RT values
-    tformMoved2Anchor.R = cell2mat(refinedPoses.Orientation(iN,1));
-    tformMoved2Anchor.T = cell2mat(refinedPoses.Location(iN,1))';
+    tformMoved2Anchor.R = cell2mat(refinedRT.Orientation(iN,1));
+    tformMoved2Anchor.T = cell2mat(refinedRT.Location(iN,1))';
     
     % Write the RT values into the file
     WriteRT(tformMoved2Anchor, rtFullName); 
 end
 disp('Updated all the R|T values in the rt*_.txt files');
 
-% Output:
+% Outputs
+% =======
 refinedStruct = struct('xyz', xyzRefinedts, 'rt', refinedRT);
 rawStruct = struct('xyz', xyzRaw, 'rt', rtRaw);
 end
@@ -204,35 +231,23 @@ TF = false;
 if ~istable(matchInfo)
     % Expecting a table
     error('This argument should be a table.');
-elseif ~all(ismember({'Name', 'Matched_Points'}, matchInfo.Properties.VariableNames))
+elseif ~all(ismember({'Anchor', 'Moved', 'Matched_Points'}, ...
+        matchInfo.Properties.VariableNames))
     % Expecting the table-column members as "Name" and "Matched_Points"
-    error("The table should contain both the columns: 'Name' and 'Matched_Points'");
+    error("The table should contain the columns: 'Anchor', 'Moved' and 'Matched_Points'");
 else
     TF = true;
 end
 end
 
-function TF = validateMatchPtsPxls(matchPtsPxls)
-% This function is going to check whether it is a cell or not, and the second
-% dimention is 2 or not. It is also expecting structures as cell members. Need
-% to find out a way to check each structure.
-TF = false;
-if ~iscell(matchPtsPxls) || ~(size(matchPtsPxls, 2) == 2)
-    % Expecting a Mx2 cell containing structures
-    error('It should be a Mx2 cell');
-else
-    TF = true;
-end
-end
-
-function TF = validateRtInfo(rtInfo)
+function TF = validateRtInfo(rtRaw)
 % This function is going to test whether the variable is a table or not. Also,
 % it will check the fields of the table too.
 TF = false;
-if ~istable(rtInfo)
+if ~istable(rtRaw)
     error("Provide a table as an input");
 elseif ~all(ismember({'ViewId', 'Orientation', 'Location'}, ...
-        rtInfo.Properties.VariableNames))
+        rtRaw.Properties.VariableNames))
     error("Provide required columns");
 else
     TF = true;
